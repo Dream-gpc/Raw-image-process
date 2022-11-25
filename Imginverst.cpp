248d20848e44efc70bb3b3555a64cf12e44d0bde
PcGao <57758908+Dream-gpc@users.noreply.github.com>
2022-11-25 10:31:22 +0800

    Create Imginverst.cpp

---
+++ Imginverst.cpp
@@ -0,0 +1,35 @@
+//***************************************************************************
+//
+// FileName:      Imginverst.cpp  
+// Author:        gaopc
+// Date:      2022/11/25
+// Description:   图像反转
+// Function List:  
+// History:              
+//          <author>      <time>       <desc>        
+//            gaopc          2022/11/25    Build
+//
+//***************************************************************************
+
+// opencv Mat作为输入
+
+void Image_inversion(cv::Mat& src, cv::Mat& dst) {
+    int nr = src.rows;
+    int nc = src.cols * src.channels();
+
+    src.copyTo(dst);
+    if (src.isContinuous() && dst.isContinuous()) {  //判断图像连续性
+        nr = 1;
+        nc = src.rows * src.cols * src.channels(); //行数*列数 * 通道数=一维数组的个数
+    }
+    for (int i = 0; i < nr; i++) {
+        const uchar* srcdata = src.ptr <uchar>(i);  //采用指针访问像素，获取第i行的首地址
+        uchar* dstdata = dst.ptr <uchar>(i);
+        //16位2字节存1像素
+        for (int j = 0; j < 2*nc; j++) {
+            
+            dstdata[j] = 65535 - srcdata[j]; //开始处理每个像素
+
+        }
+    }
+}
