28277727856b51d2703654aa9474f0113621f462
PcGao <57758908+Dream-gpc@users.noreply.github.com>
2022-12-06 14:43:47 +0800

    Update Imginverst.cpp

--- Imginverst.cpp
+++ Imginverst.cpp
@@ -23,10 +23,10 @@ void Image_inversion(cv::Mat& src, cv::Mat& dst) {
         nc = src.rows * src.cols * src.channels(); //行数*列数 * 通道数=一维数组的个数
     }
     for (int i = 0; i < nr; i++) {
-        const uchar* srcdata = src.ptr <uchar>(i);  //采用指针访问像素，获取第i行的首地址
-        uchar* dstdata = dst.ptr <uchar>(i);
-        //16位2字节存1像素
-        for (int j = 0; j < 2*nc; j++) {
+        const ushort* srcdata = src.ptr <ushort>(i);  //采用指针访问像素，获取第i行的首地址
+        ushort* dstdata = dst.ptr <ushort>(i);
+        
+        for (int j = 0; j < nc; j++) {
             
             dstdata[j] = 65535 - srcdata[j]; //开始处理每个像素
 
